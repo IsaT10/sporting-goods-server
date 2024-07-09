2e4fb871e628936a35078b13eea6ab66eb3ee5a5
IsaT10 <rakiib010@gmail.com>
2024-07-09 06:01:56 +0600

    init

---
+++ src/routes/index.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+import userRoute from '../modules/User/user.route';
+
+const router = Router();
+const moduleRoutes = [{ path: '/users', route: userRoute }];
+
+moduleRoutes.forEach((route) => router.use(route.path, route.route));
+
+export default router;
